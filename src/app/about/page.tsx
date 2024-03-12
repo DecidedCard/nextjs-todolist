7ef4eaa54b5270346f5b8ea1762280f64d91de36
DecidedCard <heajun35@gmail.com>
2024-03-12 20:10:32 +0900

    [feat] about page ssg 작성

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-const AboutPage = () => {
+const AboutPage = async () => {
+  const res = await fetch("http://localhost:3000/api/company");
+  const companyInfo = await res.json();
+  console.log(companyInfo);
   return <div>AboutPage</div>;
 };
 
