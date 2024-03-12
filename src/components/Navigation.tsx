0070fce6119dd99a8f0761f57e3e1f5713ae1637
DecidedCard <heajun35@gmail.com>
2024-03-12 19:44:39 +0900

    [style] navigation style 작성

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <nav>
+    <nav className="flex justify-evenly items-center h-16 text-2xl">
       <Link href={"/about"}>About</Link>
       <Link href={"/report"}>Report</Link>
       <Link href={"/todo-csr"}>Todo-CSR</Link>
