dd9f73c03799ad64cbbd65b04b363c19165ef9c9
DecidedCard <heajun35@gmail.com>
2024-03-12 19:37:47 +0900

    [feat] router setting

---
+++ src/app/about/todo-csr/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoCSRPage = () => {
+  return <div>TodoCSRPage</div>;
+};
+
+export default TodoCSRPage;
---
+++ src/app/todo-ssr/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoSSRPage = () => {
+  return <div>TodoSSRPage</div>;
+};
+
+export default TodoSSRPage;
