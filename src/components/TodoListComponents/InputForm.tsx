e29e563af6ca31fa5431264ec0cd88693553b9a8
DecidedCard <heajun35@gmail.com>
2024-03-13 15:12:40 +0900

    [style] inputform style 작성

--- src/components/TodoListComponents/InputForm.tsx
+++ src/components/TodoListComponents/InputForm.tsx
@@ -14,25 +14,28 @@ const InputForm = () => {
   return (
     <form
       onSubmit={onSubmitHandler}
-      className="flex justify-center items-center gap-10 h-14"
+      className="flex flex-col justify-between items-center m-2 p-2 h-32"
     >
-      <input
-        type="text"
-        placeholder="제목을 입력해주세요"
-        value={title}
-        onChange={onChamgeTitleHandler}
-        className="border border-solid border-slate-950 rounded-3xl p-2 w-72 h-10"
-      />
-      <input
-        type="text"
-        placeholder="내용을 입력해주세요"
-        value={contents}
-        onChange={onChamgeContentsHandler}
-        className="border border-solid border-slate-950 rounded-3xl p-2 w-72 h-10"
-      />
-      <button type="submit" className="bg-green-400 w-14 h-7 rounded-2xl">
-        등록
-      </button>
+      <h2 className="text-2xl">TodoList를 작성해 주세요!</h2>
+      <div className="flex items-center gap-10">
+        <input
+          type="text"
+          placeholder="제목을 입력해주세요"
+          value={title}
+          onChange={onChamgeTitleHandler}
+          className="border border-solid border-slate-950 rounded-3xl p-2 w-72 h-10"
+        />
+        <input
+          type="text"
+          placeholder="내용을 입력해주세요"
+          value={contents}
+          onChange={onChamgeContentsHandler}
+          className="border border-solid border-slate-950 rounded-3xl p-2 w-72 h-10"
+        />
+        <button type="submit" className="bg-green-400 w-14 h-7 rounded-2xl">
+          등록
+        </button>
+      </div>
     </form>
   );
 };
