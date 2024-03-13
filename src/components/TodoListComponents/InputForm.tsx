610865e267fffca7e48079df679f2900cfaad5f5
DecidedCard <heajun35@gmail.com>
2024-03-13 18:52:05 +0900

    [fix] 오타 수정

--- src/components/TodoListComponents/InputForm.tsx
+++ src/components/TodoListComponents/InputForm.tsx
@@ -6,9 +6,9 @@ import React from "react";
 const InputForm = () => {
   const {
     title,
-    onChamgeTitleHandler,
+    onChangeTitleHandler,
     contents,
-    onChamgeContentsHandler,
+    onChangeContentsHandler,
     onSubmitHandler,
   } = useTodoInputForm();
   return (
@@ -16,20 +16,20 @@ const InputForm = () => {
       onSubmit={onSubmitHandler}
       className="flex flex-col justify-between items-center m-2 p-2 h-32"
     >
-      <h2 className="text-2xl">TodoList를 작성해 주세요!</h2>
+      <h2 className="text-2xl text-white">TodoList를 작성해 주세요!</h2>
       <div className="flex items-center gap-10">
         <input
           type="text"
           placeholder="제목을 입력해주세요"
           value={title}
-          onChange={onChamgeTitleHandler}
+          onChange={onChangeTitleHandler}
           className="border border-solid border-slate-950 rounded-3xl p-2 w-72 h-10"
         />
         <input
           type="text"
           placeholder="내용을 입력해주세요"
           value={contents}
-          onChange={onChamgeContentsHandler}
+          onChange={onChangeContentsHandler}
           className="border border-solid border-slate-950 rounded-3xl p-2 w-72 h-10"
         />
         <button type="submit" className="bg-green-400 w-14 h-7 rounded-2xl">
