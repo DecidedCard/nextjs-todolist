aaa40390c32b8b12317fcefc35bc0c60fe3405ae
DecidedCard <heajun35@gmail.com>
2024-03-13 09:44:23 +0900

    [feat] ssr page 작성

--- src/app/todo-ssr/page.tsx
+++ src/app/todo-ssr/page.tsx
@@ -1,7 +1,24 @@
+import { TodoListType } from "@/types";
 import React from "react";
 
-const TodoSSRPage = () => {
-  return <div>TodoSSRPage</div>;
+const TodoSSRPage = async () => {
+  const res = await fetch("http://localhost:3000/api/todoList", {
+    cache: "no-cache",
+  });
+  const { todoList }: TodoListType = await res.json();
+
+  return (
+    <main>
+      {todoList.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.contents}</p>
+          </div>
+        );
+      })}
+    </main>
+  );
 };
 
 export default TodoSSRPage;
