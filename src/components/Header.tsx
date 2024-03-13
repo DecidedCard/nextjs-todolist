8a81f41e18b34253801be08bf9cf91732db85ada
DecidedCard <heajun35@gmail.com>
2024-03-13 15:23:27 +0900

    [feat] header Link 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
     <header className="flex justify-center items-center h-14 text-4xl font-bold">
-      <h1>Next.js TodoList</h1>
+      <Link href={"/"}>
+        <h1>Next.js TodoList</h1>
+      </Link>
     </header>
   );
 };
