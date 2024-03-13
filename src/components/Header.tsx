94acf0e36533f30ff33bf8f60b784b6fb6f6563c
DecidedCard <heajun35@gmail.com>
2024-03-13 11:51:18 +0900

    [style] header 작성

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-center items-center h-14 text-3xl font-bold">
+      Next.js TodoList
+    </header>
+  );
+};
+
+export default Header;
